2ac8f1996b880d06de1e6277fc2c435635be3602
HuTao <157451486+Kim1an@users.noreply.github.com>
2024-03-05 15:43:52 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,6 @@
+num1 = int(input("Entyer 1st number: "))
+num2 = int(input("Entyer 2nd number: "))
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
---
+++ output.py
@@ -0,0 +1,10 @@
+x = 25
+message = "Yoow"
+ratio = 3.14
+
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+print(message + " my name is Me. I am " + str(x) + " years old.")
+print(f"{message} my name is Me. I am {x} years old.")
